6a857f70bde39a03afa33f69c1af94549f54ee1e
mrfams <famara2ray11@gmail.com>
2026-08-16 00:55:59 +0300

    fix(hooks): use CJS commitlint config + non-blocking pre-push
    - commitlint.config.js: module.exports instead of export default
    - .husky/pre-push: add shebang, source husky.sh, || true for DB-less envs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,11 +1,7 @@
-export default {
+module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']],
     'subject-case': [0],
   },
 };
